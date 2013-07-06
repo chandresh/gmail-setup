80a6dce1cab25c0c70256fbcabd0dea61d7dcda8
Chandresh Pant <cp@cpant.in>
2013-07-06 06:38:49 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmail::Application.config.secret_token = '44a067ad02499511401ba7c56ae522f317a516c9bb016f42be0b5817caeb2fa78ae892750d8cfc1978f63c22e9efd20d696272bcf1bcb684e5e1d862e713c84b'
